ca7eda1da46a39ac1639705318e59837ce32c279
ArshMarsh <arshiabakhshayesh@yahoo.com>
2024-08-04 13:39:18 +0300

    testing bedrock access

--- amplify/backend/function/quizFlow/src/index.py
+++ amplify/backend/function/quizFlow/src/index.py
@@ -1,4 +1,34 @@
 import json
+import boto3
+
+bedrock_client = boto3.client(service_name="bedrock-runtime", region_name="eu-central-1")
+
+
+def call_llm(prompt):
+    model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
+    response = bedrock_client.invoke_model(
+        modelId=model_id,
+        body=json.dumps(
+            {
+                "anthropic_version": "bedrock-2023-05-31",
+                "max_tokens": 1000,
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": [
+                            {
+                                "type": "text",
+                                "text":"say hello in the most lengthy way(10 words)"
+                            }
+                        ],
+                    }
+                ],
+            }
+        ),
+    )
+    response_body = response["body"].read()
+    response_text = json.loads(response_body.decode("utf-8"))
+    return response_text
 
 def handler(event, context):
   print('received event:')
@@ -10,5 +40,5 @@ def handler(event, context):
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
       },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
+      'body': json.dumps(call_llm("sth"))
   }
