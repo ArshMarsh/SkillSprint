1c53094e4c42f27842eb752b303722aa26f304d9
ArshMarsh <arshiabakhshayesh@yahoo.com>
2024-08-12 23:46:43 +0300

    ip aws test bug fix

--- amplify/backend/function/testfunc/src/index.py
+++ amplify/backend/function/testfunc/src/index.py
@@ -9,13 +9,13 @@ def handler(event, context):
     testret = {}
     for i, result in enumerate(searchs):
         testret[i] = result
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps(testret)
-  }
+
+    return {
+        'statusCode': 200,
+        'headers': {
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps(testret)
+    }
